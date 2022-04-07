eb4ed276a63f31c0a4e88b1874b6a77a097d215f
Trukhaibit <70858117+Trukhaibit@users.noreply.github.com>
2022-04-07 11:54:32 -0400

    Add files via upload

---
+++ Test GP App/Test GP App/Page3.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Globalization;
+
+
+namespace Test_GP_App
+{
+    /// <summary>
+    /// Interaction logic for Page3.xaml
+    /// </summary>
+    public partial class Page3 : Page
+    {
+        public Page3()
+        {
+            InitializeComponent();
+
+            setText();
+        }
+
+        public void setText()
+        {
+            /// Grabs current time, adds five minutes and plonks it onto the first button
+
+            DateTime localTime = DateTime.Now;
+            localTime = localTime.AddMinutes(5);
+            asapText.Text = "Pick up ASAP:\n" + localTime.ToString("h:mm tt");
+        }
+    }
+}
