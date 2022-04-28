601a63ba2fd5463bcba9c7d89ba0cb17713f382f
BlubberboomM <77407933+BlubberboomM@users.noreply.github.com>
2022-04-28 12:13:07 -0400

    Add files via upload

---
+++ Test GP App/Page4.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test_GP_App
+{
+    /// <summary>
+    /// Interaction logic for Page4.xaml
+    /// </summary>
+    public partial class Page4 : Page
+    {
+        string localtime;
+        public Page4(string time)
+        {
+            InitializeComponent();
+
+            
+
+            localtime = time;
+            pickupText.Text = "Pickup Time:\n" + localtime;
+
+        }
+
+        private void back(object sender, MouseButtonEventArgs e)
+        {
+            this.NavigationService.Navigate(new Page3());
+        }
+
+        private void pay_Now(object sender, MouseButtonEventArgs e)
+        {
+            string name = nameBox.Text;
+            File.WriteAllText("Names.txt", name);
+            string phone = phoneBox.Text;
+            File.WriteAllText("Phones.txt", phone);
+            double total = 20.00;
+            this.NavigationService.Navigate(new Page5(name, phone, total, localtime));
+        }
+
+        private void pay_Pickup(object sender, MouseButtonEventArgs e)
+        {
+            this.NavigationService.Navigate(new Page6(localtime));
+        }
+    }
+}
---
+++ Test GP App/Page6.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test_GP_App
+{
+    /// <summary>
+    /// Interaction logic for Page6.xaml
+    /// </summary>
+    public partial class Page6 : Page
+    {
+        string localtime;
+
+        public Page6(string time)
+        {
+            InitializeComponent();
+
+            localtime = time;
+            ReadyText.Text = "Your order will be ready at:\n" + localtime;
+        }
+
+        private void CloseAppButtonClick(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.ShutdownMode = ShutdownMode.OnExplicitShutdown;
+            Application.Current.Shutdown();
+        }
+
+        private void ReturnToMenuButtonClick(object sender, MouseButtonEventArgs e)
+        {
+            this.NavigationService.Navigate(new Page1());
+        }
+
+        private void ReturnToMenuTextClick(object sender, MouseButtonEventArgs e)
+        {
+            this.NavigationService.Navigate(new Page1());
+        }
+
+        private void CloseAppTextClick(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.ShutdownMode = ShutdownMode.OnExplicitShutdown;
+            Application.Current.Shutdown();
+        }
+    }
+}
